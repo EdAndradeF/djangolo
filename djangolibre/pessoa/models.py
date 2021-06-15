1badc0f854175327273a1ccc05570233ee462fd5
emmanuel.marinho@intelie.com.br <emmanuel.marinho@intelie.com.br>
2021-06-14 22:16:35 -0300

    adcionada classe aluno

--- djangolibre/pessoa/models.py
+++ djangolibre/pessoa/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
 class Pessoa(models.Model):
     nome = models.CharField(max_length=12, null=False)
     idade = models.IntegerField()
+
+
+class Aluno(Pessoa):
+    nota = models.FloatField()
